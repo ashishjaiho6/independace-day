3231ca6602d5125c396c3ac46dcebaf0c2c98117
Ashish Gupta <ashish@bloohash.com>
2023-08-14 16:35:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// // script.js
+// document.addEventListener("DOMContentLoaded", function () {
+//   const scene = document.querySelector(".scene");
+
+//   scene.addEventListener("click", function () {
+//     // Create and animate additional elements or effects
+//   });
+// });
